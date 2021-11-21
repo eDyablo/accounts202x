9025121804e74a74fdde70d728ac18b397309c1f
eDyablo <eyablonsky@gmail.com>
2021-11-21 12:46:38 +0200

    basic account api

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, abort, request
 from flask_migrate import Migrate
 from flask.json import jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Account
 from os import environ
-from models import db
 
 # Make sure database URL is set
 if not environ.get("DATABASE_URL"):
@@ -18,6 +19,21 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 
-@app.route('/api/v1.0/accounts', methods=['GET'])
+@app.route('/api/v1.0/accounts', methods=['GET', 'POST'])
 def accounts():
-    return jsonify({})
+    if request.method == 'POST':
+        if not request.json or not 'name' in request.json:
+            abort(400)
+        account = Account(name=request.json['name'])
+        db.session.add(account)
+        db.session.commit()
+        return jsonify({'account': account.toJSON()}), 201
+    elif request.method == 'GET':
+        accounts = db.session.query(Account).all()
+        return jsonify({'accounts': [account.toJSON() for account in accounts]})
+
+
+@app.route('/api/v1.0/accounts/<account_id>', methods=['GET'])
+def get_account(account_id):
+    account = db.session.query(Account).get(account_id)
+    return jsonify({'account': account.toJSON()})
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
+import json
 
 db = SQLAlchemy()
 
@@ -10,5 +11,14 @@ class Account(db.Model):
     __tablename__ = 'accounts'
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    name = db.Column(db.String(128), unique=True)
+    name = db.Column(db.String(80), unique=True)
     description = db.Column(db.Text())
+
+    def serialized(self):
+        return {
+            'id': str(self.id),
+            'name': self.name,
+        }
+
+    def toJSON(self):
+        return json.dumps(self.serialized())
